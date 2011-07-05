68066d5f107bb6805dea1b6f9e3392bbebcefbbb
Zeno Gantner <zeno.gantner@gmail.com>
2011-07-05 17:25:54 +0200

    add more scripts, fix problem with hyperparameters

--- Makefile
+++ Makefile
@@ -1,19 +1,23 @@
 MYMEDIA_DIR=$(CURDIR)/../mymedialite/src/RatingPrediction/bin/Debug
 
-ZIP=bipolar-slopeone.zip bmf.zip global-average.zip item-average.zip item-knn-cosine.zip item-knn-pearson.zip slopeone.zip user-average.zip user-knn-cosine.zip user-knn-pearson.zip
+ZIP=bipolar-slopeone.zip bmf-10.zip bmf-20.zip bmf-40.zip bmf-5.zip bmf-60.zip factorwise-mf.zip global-average.zip item-average.zip item-knn-cosine.zip item-knn-pearson.zip mf-10.zip mf-20.zip mf-40.zip mf-5.zip mf-60.zip slopeone.zip user-average.zip user-knn-cosine.zip user-knn-pearson.zip
 
 get-mymedialite:
 	cp $(MYMEDIA_DIR)/*.dll .
 	cp $(MYMEDIA_DIR)/*.exe .
 	cp $(MYMEDIA_DIR)/*.mdb .
-	rm MathNet.Iridium.dll
+	rm MathNet.Iridium.dll	
 
 all: $(ZIP)
 
 %.zip: %.sh
+	./$<
 	cp $< run
-	zip $@ *.dll *.mdb *.exe README run
+	touch hp.txt
+	./$< setHyperparameter 1
+	zip $@ *.dll *.mdb *.exe README run hp.txt
 
 clean:
 	rm -f *.zip
 	rm -f run
+	rm -f hp.txt
---
+++ mf-60.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+recommender=MatrixFactorization
+k=60
+
+if [ "$1" == "construct" ]; then
+    echo Nothing to construct.
+elif [ "$1" == "setHyperparameter" ]; then
+    if [ "$2" == "0.01" ]; then
+        echo --recommender-options=\"regularization=0.01\" > hp.txt
+    elif [ "$2" == "0.1" ]; then
+        echo --recommender-options=\"regularization=0.025\" > hp.txt
+    elif [ "$2" == "1" ]; then
+        echo --recommender-options=\"regularization=0.05\" > hp.txt
+    elif [ "$2" == "10" ]; then
+        echo --recommender-options=\"regularization=0.075\" > hp.txt
+    elif [ "$2" == "100" ]; then
+        echo --recommender-options=\"regularization=0.1\" > hp.txt
+    else
+        echo Unknown hyperparameter.
+    fi
+elif [ "$1" == "learn" ]; then
+    xargs --arg-file=hp.txt mono --debug RatingPrediction.exe --training-file=$2 --recommender=$recommender --recommender-options="num_factors=$k learn_rate=0.005 num_iter=125" --save-model=model.txt
+    cp $2 training_file
+elif [ "$1" == "predict" ]; then
+    mono --debug RatingPrediction.exe --training-file=training_file --test-file=$2 --prediction-file=$3 --recommender=$recommender --load-model=model.txt --prediction-line=\"{2}\"
+else
+    echo Usage: $0 construct\|setHyperparameter\|learn\|predict
+fi
